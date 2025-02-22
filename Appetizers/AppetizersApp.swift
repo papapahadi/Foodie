5cb8fe9bf67ad5e46fa60c2a1a22b41f6b519e1b
Jatin Singh <singhjatin1812@gmail.com>
2025-02-23 02:22:40 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Jatin Singh on 23/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppetizersTests/AppetizersTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersTests.swift
+//  AppetizersTests
+//
+//  Created by Jatin Singh on 23/02/25.
+//
+
+import Testing
+@testable import Appetizers
+
+struct AppetizersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
